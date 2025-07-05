9356f4950ca40271a7e7be0d952211bc53deaccc
srihariInnalytics <srihari@innalytics.in>
2025-07-05 17:10:23 +0530

    sru

--- controllers/SearchViewControllers.js
+++ controllers/SearchViewControllers.js
@@ -1,16 +1,22 @@
+import { RiAwardLine } from 'react-icons/ri';
 import { UserList } from '../models/LoginModels.js'
 
 export const getSearchView = async (req, res) => {
     try {
         const raw = req.query.data;
+        console.log(raw);
         const { search_type } = JSON.parse(raw); // parse first!
-        console.log(search_type); // sri 10
+        console.log(search_type);
+         // sri 10
 
         if (search_type === 'user_list') {
             const userlist = await UserList.find(); // ✅ await!
-            return res.status(200).json({ data: userlist });
+            return res.status(200).json({ success : true , data: userlist });
+        }
+        else{
+            return res.json({ success : false ,  error: 'Pass correct Parameter' });
         }
     } catch (err) {
-        res.status(400).json({ error: 'Invalid JSON format in query param' });
+        res.status(400).json({ success : false ,  error: 'Invalid JSON format in query param' });
     }
 };
